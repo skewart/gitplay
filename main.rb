85b50a3aded1d17ab97c4a36c6f7638f5c6d754f
Scott Ewart <scott.ewart@gmail.com>
2016-02-07 13:50:19 -0800

    Basic exploration

--- main.rb
+++ main.rb
@@ -1,7 +1,23 @@
 require 'dotenv'
-require 'github'
+require 'github_api'
 
 Dotenv.load
 
 puts "Hello, World."
 
+# $Github = Github.new
+
+def list_pulls
+  github = Github.new user: 'peter-murach', repo: 'github'
+  
+  github.repos.comments.list do |com|
+    puts com.body
+  end
+
+  # # Lists comments on PRs not yet merged in to the Repo, or from external repos
+  # github.pull_requests.comments.list do |com|
+  #   puts com.body
+  # end
+end
+
+list_pulls
